a254dcc4610eacb7dbd059b3bea438cbb3b91416
Samson Aderibigbe <samsontimilehin@gmail.com>
2022-01-25 22:54:33 +0100

    update

---
+++ web/src/test/java/com/oneplug/multiModuleTest/repository/AccountHolderRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.oneplug.multiModuleTest.repository;
+
+import account.AccountHolder;
+import accountRepo.AccountHolderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class AccountHolderRepositoryTest {
+
+
+//        @Autowired
+//        private AccountHolderRepository accountHolderRepository;
+
+        private AccountHolder accountHolder;
+
+        @BeforeEach
+        void setUp() {
+            accountHolder = new AccountHolder();
+        }
+
+        @Test
+        void saveAccountHolder() {
+//            accountHolderRepository.save(accountHolder);
+//            AccountHolder fetchAccountHolder = accountHolderRepository.findById(accountHolder.getId()).get();
+//            assertEquals(1, fetchAccountHolder.getId());
+        }
+}
